9945a6d05cd844669a3bde1727c0424c8b7aa2a7
John Huang <johnhuang@Johns-MacBook-Pro-2.local>
2021-10-10 15:37:32 -0700

    Add true root to webflix rails react view template

---
+++ app/javascript/components/Root.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Webflix Root</h1>
+        <p className="lead">
+          Webflix Root Page
+        </p>
+        <hr className="my-4" />
+      </div>
+    </div>
+  </div>
+);
