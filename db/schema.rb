ac084b112ff82e778706aa9c5968c2ac02c8ffca
14jthaxton <14jthaxton@gmail.com>
2021-10-31 15:35:42 -0700

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,24 +32,4 @@ ActiveRecord::Schema.define(version: 2021_10_17_224636) do
     t.index ["jti"], name: "index_jwt_blacklist_on_jti"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "session_token"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
